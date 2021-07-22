7a29a89a3a029d185a09301e2ba8310e33bb253e
Juan Pablo Artola <juanpabloartola@gmail.com>
2021-07-22 12:24:41 -0300

    added web socket

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,12 +1,15 @@
 package com.jetbrains.handson.httpapi
 
+import com.jetbrains.handson.chat.server.Connection
 import io.ktor.application.Application
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
+import io.ktor.http.cio.websocket.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
+import io.ktor.websocket.*
 import models.customerStorage
 import models.orderStorage
 import org.jetbrains.exposed.sql.*
@@ -14,8 +17,12 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import persistence.*
 import routes.registerCustomerRoutes
 import routes.registerOrderRoutes
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+
 
 fun Application.module() {
     install(ContentNegotiation) {
@@ -46,6 +53,36 @@ fun Application.module() {
         customerStorage.addAll(CustomerEntity.all().map { it.toCustomer()})
         orderStorage.addAll(OrderEntity.all().map { it.toOrder()})
     }
+
+    //websocket
+
+    install(WebSockets)
+
+    routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/chat") {
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
+            }
+        }
+    }
+
 
 }
 
